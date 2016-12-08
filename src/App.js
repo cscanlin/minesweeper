b4a4a8899d2167ae28266f985b28554b06f20f8a
Chris Scanlin <cscanlin@gmail.com>
2016-12-08 01:45:14 -0800

    working minesweeper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Cell from './components/cell';
+
+const adjacentDirections = {
+  N: {x: 0, y:1},
+  NE: {x: 1, y:1},
+  E: {x: 1, y:0},
+  SE: {x: 1, y:-1},
+  S: {x: 0, y:-1},
+  SW: {x: -1, y:-1},
+  W: {x: -1, y:0},
+  NW: {x: -1, y:1},
+}
 
 class App extends Component {
   constructor(props) {
@@ -12,10 +21,24 @@ class App extends Component {
       numMines: 10,
       cellData: [],
     }
+    this.exploreCell = this.exploreCell.bind(this)
   }
 
   componentDidMount() {
     this.setState({cellData: this.populateInitialCellData()})
+  }
+
+  getAdjacentCells(centerCell, allCells) {
+    var adjacentCells = []
+    for (var direction in adjacentDirections) {
+      var xDiff = adjacentDirections[direction].x
+      var yDiff = adjacentDirections[direction].y
+      var adjCell = allCells.find(adjCell => adjCell.x === centerCell.x + xDiff && adjCell.y === centerCell.y + yDiff)
+      if (adjCell) {
+        adjacentCells.push(adjCell)
+      }
+    }
+    return adjacentCells
   }
 
   populateInitialCellData() {
@@ -24,27 +47,50 @@ class App extends Component {
     var spacesRemaining = this.state.gridWidth * this.state.gridHeight
     for (var h = 0; h < this.state.gridHeight; h++) {
       for (var w = 0; w < this.state.gridWidth; w++) {
-        var isMine = (Math.random() <= spacesRemaining / minesNeeded) && minesNeeded > 0
-        cells.push({x: w, y: h, isMine: isMine, isExplored: false})
+        var isMine = (Math.random() <= minesNeeded / spacesRemaining) && minesNeeded > 0
+        cells.push({x: w, y: h, isMine: isMine, isExplored: false, isFlagged: false, numAdjacent: null})
         if (isMine) {
           minesNeeded -=1
         }
         spacesRemaining -=1
       }
     }
-    return cells
+    return this.setNumAdjacent(cells)
+  }
+
+  setNumAdjacent(allCells) {
+    return allCells.map(cell => {
+      var numAdjacent = this.getAdjacentCells(cell, allCells).filter(adjCell => adjCell.isMine).length
+      cell.numAdjacent = numAdjacent ? numAdjacent: null
+      return cell
+    })
+  }
+
+  exploreCell(x, y) {
+    var cellData = this.state.cellData
+    var cellIndex = cellData.findIndex(cell => cell.x === x && cell.y === y)
+    cellData[cellIndex].isExplored = true
+    this.setState({cellData : cellData})
+    var cell = cellData[cellIndex]
+    if (!cell.numAdjacent && !cell.isMine) {
+      this.getAdjacentCells(cell, this.state.cellData).filter(adjCell => !adjCell.isExplored).forEach(adjCell =>
+        this.exploreCell(adjCell.x, adjCell.y)
+      )
+    }
+    if (cell.isMine) {
+      console.log('game over!');
+    }
   }
 
   render() {
-    var cells = this.state.cellData
-    const rowsNums = Array(this.state.gridHeight).fill().map((_, i) => i)
+    const rowsNums = Array(this.state.gridHeight).fill().map((_, row) => row)
     return (
       <div className="App">
         <div className="grid">
-          {rowsNums.map((r) =>
-            <div className="row">
-              {cells.filter(cell => cell.y === r).sort((a, b) => a.x > b.x).map((cell, i) =>
-                <Cell key={i} {...cell} />
+          {rowsNums.map((row) =>
+            <div key={row} className="row">
+              {this.state.cellData.filter(cell => cell.y === row).sort((a, b) => a.x > b.x).map((cell, col) =>
+                <Cell key={(row, col)} {...cell} exploreCell={this.exploreCell}/>
               )}
             </div>
           )}
--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 
 class Cell extends Component {
 
+  onClick() {
+    return this.props.exploreCell(this.props.x, this.props.y)
+  }
+
+  cellText() {
+    return this.props.isMine ? 'X' : this.props.numAdjacent
+  }
+
+  cellBackgroundColor() {
+    if (this.props.isMine && this.props.isExplored) {
+      return '#ff0000'
+    } else {
+      return this.props.isExplored ? '#bfbfbf' : '#ededed'
+    }
+  }
+
   render() {
+    const cellStyle = {
+      backgroundColor: this.cellBackgroundColor()
+    }
     return (
-      <div className="Cell">
-        <span>({this.props.x}, {this.props.y})</span>
+      <div className="cell" style={cellStyle} onClick={() => this.onClick()}>
+        { this.props.isExplored ? <span className="cell-text">{this.cellText()}</span> : null }
       </div>
     );
   }
@@ -16,6 +35,8 @@ Cell.propTypes = {
   y: React.PropTypes.number.isRequired,
   isMine: React.PropTypes.bool.isRequired,
   isExplored: React.PropTypes.bool.isRequired,
+  numAdjacent: React.PropTypes.number,
+  exploreCell: React.PropTypes.func.isRequired,
 }
 
 export default Cell;
