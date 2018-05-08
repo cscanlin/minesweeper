e31f209158ab09b7f321ed261eafb16c7a8b7c8b
Chris Scanlin <cscanlin@gmail.com>
2018-05-07 20:59:32 -0700

    add directions, improve css, add min/max

--- src/components/OptionField.jsx
+++ src/components/OptionField.jsx
@@ -6,12 +6,14 @@ class OptionField extends Component {
   render() {
     return (
       <div className="option">
-        <label htmlFor={this.props.fieldName}>{this.props.fieldName}</label>
+        <label htmlFor={this.props.fieldName}>{`${this.props.fieldName}: `}</label>
         <input
           id={this.props.fieldName}
           type={this.props.inputType}
           className={'additional-options-input'}
           defaultValue={this.props.fieldValue}
+          min={this.props.min}
+          max={this.props.max}
           onBlur={this.props.onBlur}
           onChange={this.props.onChange}
         />
@@ -26,6 +28,8 @@ OptionField.propTypes = {
     PropTypes.number,
     PropTypes.bool,
   ]).isRequired,
+  min: PropTypes.number,
+  max: PropTypes.number,
   inputType: PropTypes.string.isRequired,
   onBlur: PropTypes.func,
   onChange: PropTypes.func,
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -5,22 +5,29 @@ import OptionField from './OptionField'
 
 class Options extends Component {
 
-  numberOptionProps() {
-    return ['numMines','gridWidth', 'gridHeight']
+  numberOptionMap() {
+    return {
+      gridWidth: {min: 5, max: 40},
+      gridHeight: {min: 5, max: 30},
+      numMines: {min: 1, max: this.props.gridWidth * this.props.gridHeight - 1},
+    }
   }
 
   render() {
+    const numberOptionMap = this.numberOptionMap()
     return (
       <form className="additional-options-container">
-        {this.numberOptionProps().map(option => {
-          return <OptionField
+        {Object.entries(numberOptionMap).map(([option, optionConf]) => (
+          <OptionField
             key={option}
             fieldName={option}
             inputType='number'
             fieldValue={this.props[option]}
+            min={optionConf.min}
+            max={optionConf.max}
             onBlur={this.props.resetGame}
           />
-        })}
+        ))}
         <OptionField
           key='showAllMines'
           fieldName='showAllMines'
